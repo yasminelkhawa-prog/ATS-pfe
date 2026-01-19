4f7ce259fef764d3d7289dddee3ff07bc1de7f2c
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 15:55:09 +0100

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,6 @@
-<script setup lang="ts">
-useHead({
-  bodyAttrs: {
-    class: 'font-lato',
-  },
-});
-</script>
-
 <template>
-  <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div style="background-color: white; color: black; padding: 50px; text-align: center;">
+    <h1>TEST DE FONCTIONNEMENT</h1>
+    <p>Si vous voyez ce texte, c'est que le site marche !</p>
+  </div>
 </template>
