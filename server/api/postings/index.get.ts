d3cc04a2e217c31cb452dad138a16e847377a51d
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 04:28:43 +0100

    Create index.get.ts

---
+++ server/api/postings/index.get.ts
@@ -0,0 +1,30 @@
+import { type JobPosting, jobPostingsTable } from '../../db/schema';
+import authenticateAdminRequest from '../../utils/admin';
+import { desc } from 'drizzle-orm';
+
+/**
+ * DB lookup for totalApplicants and cache lookup for everything else.
+ */
+export default defineEventHandler(async (event) => {
+  await authenticateAdminRequest(event);
+
+  const database = await useDatabase();
+
+  const totalApplicantsRecord = await database
+    .select({
+      id: jobPostingsTable.id,
+      totalApplicants: jobPostingsTable.totalApplicants,
+    })
+    .from(jobPostingsTable)
+    .orderBy(desc(jobPostingsTable.createdAt));
+
+  const totalApplicantsById: Record<string, number> = {};
+  totalApplicantsRecord.forEach((tar) => (totalApplicantsById[tar.id] = tar.totalApplicants));
+
+  const postings = (await general_memoryStorage.getItem<JobPosting[]>('postings')) || [];
+  return postings.map((p) => ({
+    ...p,
+    contents: null,
+    totalApplicants: totalApplicantsById[p.id] || 0,
+  }));
+});
