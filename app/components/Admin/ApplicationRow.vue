486c0081fa9c7ec780d8418922a9a0437977d56c
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 02:34:51 +0100

    Add ApplicationRow component for applicant display

---
+++ app/components/Admin/ApplicationRow.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  applicant: Applicant;
+  application: Application;
+}>();
+
+const candidate = props.applicant.candidate;
+</script>
+
+<template>
+  <div
+    class="flex bg-white border border-zinc-200 rounded-xl items-center justify-between py-2 px-4"
+    v-if="props.applicant && candidate"
+  >
+    <div class="flex">
+      <div class="font-medium text-zinc-800">
+        <span class="font-bold">
+          {{ candidate.firstName + ' ' + candidate.lastName }}
+        </span>
+        <br />
+        <span class="text-zinc-700">{{ candidate.email }}</span>
+      </div>
+    </div>
+    <div class="flex items-center justify-between space-x-20">
+      <div class="whitespace-nowrap">
+        <div class="text-left">
+          {{ timeAgo(new Date(application.createdAt)) }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
