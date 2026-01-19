67ef539de1c6518fd4cb3c1634921c5438550107
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 05:08:42 +0100

    Create application.ts

---
+++ shared/schemas/application.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const applicationCreateSchema = z.object({
+  postingId: z.string().uuid(),
+});
+
+export const applicationsLookupSchema = z.object({
+  postingIds: z.array(z.string().uuid()),
+});
