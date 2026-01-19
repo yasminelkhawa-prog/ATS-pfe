68f0943a07d80ca152502992800b9c36159de135
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 02:57:53 +0100

    Add files via upload

---
+++ app/components/Onboarding/KeyInput.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const emits = defineEmits<{
+  done: [string, boolean];
+}>();
+
+const key = ref('');
+const error = ref('');
+
+const isVerifying = ref(false);
+const verify = async () => {
+  if (!key.value) return;
+  try {
+    isVerifying.value = true;
+    const response = await $fetch<{ result: boolean; registrationNeeded: boolean }>('/api/onboarding/validate-key', {
+      method: 'POST',
+      body: { key: key.value },
+    });
+    if (response.result) {
+      emits('done', key.value, response.registrationNeeded);
+    } else {
+      error.value = 'Invalid Key. Try again.';
+    }
+  } catch (e) {
+    console.error('Error checking key status', e);
+    error.value = 'Some error occured. Try again later.';
+  } finally {
+    isVerifying.value = false;
+  }
+};
+</script>
+<template>
+  <section class="flex items-center justify-center min-h-screen">
+    <div class="max-w-sm mx-auto w-full text-center">
+      <div class="justify-center flex">
+        <img src="/vidur-logo.svg" class="h-5" />
+      </div>
+      <h4 class="text-md text-zinc-600 mt-2 mb-6">The only recruiting software you will ever need.</h4>
+      <!-- Form -->
+      <form class="justify-center mt-10">
+        <VInputText
+          id="input-key"
+          label="To get started, enter the one-time setup code."
+          placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+          v-model="key"
+        />
+        <VFormInput
+          label="You can find the key in standard logs of the server."
+          label-class="text-xs text-zinc-400"
+          id="random"
+        />
+        <span class="text-sm font-bold text-rose-500">{{ error }}</span>
+        <div class="flex justify-center mt-6">
+          <VInputButton @click.prevent="verify" :loading="isVerifying" spinner-class="h-6">Verify</VInputButton>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
