818d257a23b778cee7cc05b2baf8b8114adc183f
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 05:02:34 +0100

    Add files via upload

---
+++ server/utils/startup/config-cache.ts
@@ -0,0 +1,14 @@
+import { createStorage } from 'unstorage';
+import memoryDriver from 'unstorage/drivers/memory';
+
+export async function configureCache() {
+  const storage = useStorage();
+
+  const defaultCache = createStorage({
+    driver: memoryDriver(),
+  });
+
+  storage.mount(MEMORY_STORAGE_KEY, defaultCache);
+
+  await seedCache();
+}
