04e92457de9f7d61ee33a32cffc4a2f673cf27cb
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 04:57:38 +0100

    Create admin.ts

---
+++ server/utils/admin.ts
@@ -0,0 +1,15 @@
+import { H3Event } from 'h3';
+import authenticateRequest from './auth';
+
+export default async function authenticateAdminRequest(event: H3Event) {
+  const session = await authenticateRequest(event);
+
+  const bypassAdmin = useRuntimeConfig().bypassAdmin;
+
+  if (bypassAdmin) {
+    console.log('Bypassing Admin Check.');
+    return session;
+  }
+
+  return session;
+}
