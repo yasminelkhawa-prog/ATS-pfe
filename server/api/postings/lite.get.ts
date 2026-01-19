a202028a2841d396d064310a1272b4906bce0667
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 04:29:14 +0100

    Add files via upload

---
+++ server/api/postings/lite.get.ts
@@ -0,0 +1,14 @@
+import { type JobPosting } from '~~/server/db/schema';
+import authenticateAdminRequest from '~~/server/utils/admin';
+
+export default defineEventHandler(async (event) => {
+  await authenticateAdminRequest(event);
+
+  const postings = (await general_memoryStorage.getItem<JobPosting[]>('postings')) || [];
+
+  if (IS_DEV) {
+    console.log('[/api/postings/lite] found', postings.length, 'postings');
+  }
+
+  return postings.map((p) => ({ id: p.id, title: p.title }));
+});
