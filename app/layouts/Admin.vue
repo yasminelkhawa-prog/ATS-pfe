67a1c243af64926611d498d968893219b43752b8
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 03:26:34 +0100

    Add Admin layout with responsive sidebar

---
+++ app/layouts/Admin.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useDisplayMedia } from '@vueuse/core';
+
+useDisplayMedia();
+</script>
+
+<template>
+  <main class="min-h-screen w-full flex">
+    <PartialSidebar class="w-[200px]" />
+    <section class="w-[calc(100%-200px)] bg-zinc-50 min-h-screen">
+      <slot />
+    </section>
+  </main>
+</template>
