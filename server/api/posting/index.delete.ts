43b8baeb6e32db80b4b5c446974da1721915c722
yasminelkhawa-prog <yasminelkhawa@gmail.com>
2026-01-19 04:26:37 +0100

    Create index.delete.ts

---
+++ server/api/posting/index.delete.ts
@@ -0,0 +1,37 @@
+import { eq } from 'drizzle-orm';
+import { type JobPosting, jobPostingsTable } from '../../db/schema';
+import authenticateAdminRequest from '../../utils/admin';
+import { deleteJobPostingSchema } from '~~/shared/schemas/posting';
+
+export default defineEventHandler(async (event) => {
+  await authenticateAdminRequest(event);
+  const q = await getValidatedQuery(event, deleteJobPostingSchema.parse);
+
+  const jobPostingId = q.id;
+
+  const database = await useDatabase();
+
+  const jobPostingsResult = await database
+    .delete(jobPostingsTable)
+    .where(eq(jobPostingsTable.id, jobPostingId))
+    .returning({ id: jobPostingsTable.id });
+
+  if (jobPostingsResult.length == 0) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Job posting not found',
+    });
+  }
+
+  const deletedPostingId = jobPostingsResult[0]?.id;
+
+  const postings = (await general_memoryStorage.getItem<JobPosting[]>('postings')) || [];
+
+  await general_memoryStorage.setItem(
+    'postings',
+    postings.filter((p) => p.id !== deletedPostingId)
+  );
+
+  const totalActivePostings = (await general_memoryStorage.getItem<number>('totalActivePostings'))!;
+  await general_memoryStorage.setItem<number>('totalActivePostings', totalActivePostings - 1);
+});
